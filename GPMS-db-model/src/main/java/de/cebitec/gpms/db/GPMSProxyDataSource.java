fb886739ff6419b38373dc8629c88ed286754834
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2020-08-07 16:52:10 +0200

    rm unused imports

--- GPMS-db-model/src/main/java/de/cebitec/gpms/db/GPMSProxyDataSource.java
+++ GPMS-db-model/src/main/java/de/cebitec/gpms/db/GPMSProxyDataSource.java
@@ -4,26 +4,9 @@ import de.cebitec.gpms.data.JDBCMasterI;
 import de.cebitec.gpms.data.ProxyDataSourceI;
 import de.cebitec.gpms.util.GPMSManagedDataSourceI;
 import java.io.PrintWriter;
-import java.sql.Array;
-import java.sql.Blob;
-import java.sql.CallableStatement;
-import java.sql.Clob;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.NClob;
-import java.sql.PreparedStatement;
-import java.sql.SQLClientInfoException;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
-import java.sql.SQLWarning;
-import java.sql.SQLXML;
-import java.sql.Savepoint;
-import java.sql.Statement;
-import java.sql.Struct;
-import java.util.Map;
-import java.util.Properties;
-import java.util.concurrent.Executor;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
